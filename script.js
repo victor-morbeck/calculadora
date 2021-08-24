92b77a8ab78d2e2113d28ba1da361df9805b37ac
victor-morbeck <victormorbeck212@gmail.com>
2021-08-24 17:22:00 -0300

    Funcao apagar um caracter por vez criada

--- script.js
+++ script.js
@@ -7,3 +7,8 @@ function clean()
 {
     document.getElementById('resultado').innerHTML = "";
 }
+function back()
+{
+    var resultado = document.getElementById("resultado").innerHTML;
+    document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length -1);
+}
